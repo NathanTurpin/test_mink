35b4834c7025db6da4ccdb3324e42205c353c4e3
NathanTurpin <turpinn.all@gmail.com>
2024-11-27 11:12:12 +0100

    fix

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\AnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,11 +9,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class AnimalController extends AbstractController
 {
     #[Route('/animals', name: 'app_animal')]
-    public function index(AnimalRepository $animalRepository): Response
+    public function index(): Response
     {
-        $animals = $animalRepository->findAll();
         return $this->render('animal/index.html.twig', [
-            'animals' => $animals,
+            'animals' => "animals",
         ]);
     }
 }
